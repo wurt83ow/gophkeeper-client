74929fb688526be44b772c45605da787af6632e2
wurt83ow <wurt83ow@yandex.ru>
2024-02-03 17:40:47 +0300

    changed the type of the id parameter to a string type

--- pkg/gksync/gksync.go
+++ pkg/gksync/gksync.go
@@ -78,7 +78,7 @@ func (s *Sync) AddData(user_id int, table string, data map[string]string) error 
 	return nil
 }
 
-func (s *Sync) UpdateData(user_id int, id int, table string, data map[string]string) error {
+func (s *Sync) UpdateData(user_id int, id string, table string, data map[string]string) error {
 	if !s.syncWithServer {
 		return nil
 	}
@@ -87,7 +87,7 @@ func (s *Sync) UpdateData(user_id int, id int, table string, data map[string]str
 		return err
 	}
 
-	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/updateData/%s/%d/%d", s.serverURL, table, user_id, id), bytes.NewBuffer(body))
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/updateData/%s/%d/%s", s.serverURL, table, user_id, id), bytes.NewBuffer(body))
 	if err != nil {
 		return err
 	}
