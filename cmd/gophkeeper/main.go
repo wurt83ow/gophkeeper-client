c016d10c57c097d7b4b1c03d88646edc443fcecb
wurt83ow <wurt83ow@yandex.ru>
2024-02-06 20:33:35 +0300

    Added logger and implemented the SyncAllWithServer method in the goroutine.

--- cmd/gophkeeper/main.go
+++ cmd/gophkeeper/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/wurt83ow/gophkeeper-client/pkg/bdkeeper"
 	"github.com/wurt83ow/gophkeeper-client/pkg/client"
 	"github.com/wurt83ow/gophkeeper-client/pkg/config"
 	"github.com/wurt83ow/gophkeeper-client/pkg/encription"
 	"github.com/wurt83ow/gophkeeper-client/pkg/gksync"
+	"github.com/wurt83ow/gophkeeper-client/pkg/logger"
 	"github.com/wurt83ow/gophkeeper-client/pkg/services"
 )
 
 func main() {
+
+	logger := logger.NewLogger()
 	option := config.NewConfig()
 	keeper := bdkeeper.NewKeeper()
 	sync := gksync.NewSync(option.ServerURL, option.SyncWithServer)
@@ -21,10 +26,26 @@ func main() {
 	}
 
 	enc := encription.NewEnc("password")
-	service := services.NewServices(keeper, sync, sync1, enc, option, option.SyncWithServer)
+	service := services.NewServices(keeper, sync, sync1, enc, option, option.SyncWithServer, logger)
 
 	// Create a background context
 	ctx := context.Background()
+
+	if err != nil {
+		fmt.Printf("Ошибка при синхронизации данных: %s\n", err)
+	}
+	go func() {
+		ticker := time.NewTicker(5 * time.Minute) //!!! Вынести в параметры
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				service.SyncAllWithServer(ctx)
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
 	gk := client.NewClient(ctx, service, enc, option)
 	defer gk.Close()
 
