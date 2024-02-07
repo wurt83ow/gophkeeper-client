c3c5c7eda105fa8eb61f2616f438e6175fef2450
wurt83ow <wurt83ow@yandex.ru>
2024-02-07 14:11:48 +0300

    added the extension field to migration table FilesData

--- pkg/bdkeeper/migrations/20240118102926_files_data.sql
+++ pkg/bdkeeper/migrations/20240118102926_files_data.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS FilesData (
     id TEXT PRIMARY KEY,
     user_id INTEGER,
     path TEXT NOT NULL,
+    extension TEXT,   
     meta_info TEXT,  
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(user_id) REFERENCES Users(id)
